28b103e767e62ff552cf7c342949e315e615d1fd
Hendrik Amler <hendrik@perun.network>
2025-03-17 22:05:48 +0100

    Fix: Error when using frontend with https

--- src/main/kotlin/HTTP.kt
+++ src/main/kotlin/HTTP.kt
@@ -7,10 +7,12 @@ import io.ktor.server.plugins.cors.routing.*
 fun Application.configureHTTP() {
     install(CORS) {
         allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Post) 
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
         allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.ContentType) 
         allowHeader("MyCustomHeader")
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
